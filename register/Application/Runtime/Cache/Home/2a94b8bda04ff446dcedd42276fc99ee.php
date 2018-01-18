e2b463ac780802a13d45d860a378bf6e04d4f942
ninggege <2543225600@qq.com>
2018-01-18 19:04:50 +0800

    little_zongjie
    常用的一些代码

---
+++ register/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <a href="<?php echo U('Index/up_drop');?>">退出</a>
+</body>
+</html>
